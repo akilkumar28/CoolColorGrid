82f8fa5dc40091c4502bd53084410302bf183da5
akilkumar28 <akil.psn@gmail.com>
2017-08-27 17:31:46 -0700

    added color grid to the view controller

--- CoolColorGrid/ViewController.swift
+++ CoolColorGrid/ViewController.swift
@@ -12,13 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let numPerRow = 15
+        let width = self.view.frame.width / CGFloat(numPerRow)
+        
+        for j in 0...30 {
+        for i in 0...numPerRow{
+            let cell = UIView()
+            cell.layer.borderColor = UIColor.black.cgColor
+            cell.layer.borderWidth = 0.5
+            cell.backgroundColor = givColor()
+            cell.frame = CGRect(x: CGFloat(i) * width, y: CGFloat(j) * width, width: width, height: width)
+            self.view.addSubview(cell)
+        }
+        }
+        
+    
     }
+    
+    func givColor() -> UIColor {
+        
+        let red = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        
+        return UIColor(red: red, green: blue, blue: green, alpha: 1)
+        
+    }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
